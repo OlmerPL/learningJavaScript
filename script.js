31dc82a61cbba8913b1ab7136bf449ed89e91257
OlmerPL <30810593+OlmerPL@users.noreply.github.com>
2017-08-07 23:55:08 +0200

    variables, part 2

--- script.js
+++ script.js
@@ -12,30 +12,40 @@
 // console.log(fullAge);
 
 //Lectures: variables 2
-var name = 'Janek';
-var lastName = 'Kowalski';
-var age = 26;
+// var name = 'Janek';
+// var lastName = 'Kowalski';
+// var age = 26;
+//
+// console.log(name + lastName + age);
+// console.log(age + age);
+//
+// var job, isMArried;
+//
+// console.log(job);
+//
+//
+// job = 'nauczyciel';
+// isMArried = false;
+//
+// console.log(name + ' is a ' + age + ' old ' + job + '. Is he married? ' + isMArried + '.');
+//
+// age = 'dwadzieścia dwa';
+// job = 'kierowca';
+//
+// console.log(name + ' is a ' + age + ' old ' + job + '. Is he married? ' + isMArried + '.');
+//
+// lastName = prompt('Jak ma na nazwisko');
+// console.log(lastName);
+//
+// alert('Achtung!');
 
-console.log(name + lastName + age);
-console.log(age + age);
+//Lecture: operators
+var now = 2016;
+var birthYear = now - 26;
 
-var job, isMArried;
+birthYear = now - 26 * 2;
+//2016 - 52 = 1964
 
-console.log(job);
+console.log(birthYear);
 
 
-
-job = 'nauczyciel';
-isMArried = false;
-
-console.log(name + ' is a ' + age + ' old ' + job + '. Is he married? ' + isMArried + '.');
-
-age = 'dwadzieścia dwa';
-job = 'kierowca';
-
-console.log(name + ' is a ' + age + ' old ' + job + '. Is he married? ' + isMArried + '.');
-
-var lastName = prompt('Jak ma na nazwisko');
-console.log(lastName)
-
-alert('Achtung!');
