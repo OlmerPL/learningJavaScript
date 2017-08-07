977af5b1290d836e4ed5d1e514ed305ba6403fbd
OlmerPL <30810593+OlmerPL@users.noreply.github.com>
2017-08-07 23:22:14 +0200

    variables, part 2

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
+//Lecture: variables
+// var name = 'Janek';
+// console.log(name);
+//
+// var lastName = 'Kowalski';
+// console.log(lastName);
+//
+// var age = 26;
+// console.log(age);
+//
+// var fullAge = true;
+// console.log(fullAge);
+
+//Lectures: variables 2
 var name = 'Janek';
-console.log(name);
-
 var lastName = 'Kowalski';
-console.log(lastName);
-
 var age = 26;
-console.log(age);
 
-var fullAge = true;
-console.log(fullAge);
+console.log(name + lastName + age);
+console.log(age + age);
+
+var job, isMArried;
+
+console.log(job);
+
+
+
+job = 'nauczyciel';
+isMArried = false;
+
+console.log(name + ' is a ' + age + ' old ' + job + '. Is he married? ' + isMArried + '.');
+
+age = 'dwadzieścia dwa';
+job = 'kierowca';
+
+console.log(name + ' is a ' + age + ' old ' + job + '. Is he married? ' + isMArried + '.');
+
+var lastName = prompt('Jak ma na nazwisko');
+console.log(lastName)
+
+alert('Achtung!');
